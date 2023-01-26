494fe16514837d992a2e0f321a0f1fefdc85f21a
bonnie-gaggle <bonnie@gaggle.net>
2023-01-25 16:11:30 -0800

    display hard-coded playlist name + tracks

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -12,6 +12,12 @@ class App extends React.Component {
         {name: 'Name 1', artist: 'Artist 1', album: 'Album 1', id: 1},
         {name: 'Name 2', artist: 'Artist 2', album: 'Album 2', id: 2},
         {name: 'Name 3', artist: 'Artist 3', album: 'Album 3', id: 3},
+      ],
+      playlistName: 'Playlist Name',
+      playlistTracks: [
+        {name: 'Song 1', artist: 'Artist 1', album: 'Album 1', id: 1},
+        {name: 'Song 2', artist: 'Blur', album: 'Album 2', id: 2},
+        {name: 'Song 3', artist: 'Artist 3', album: 'Album 3', id: 3},
       ]
     }
   }
@@ -23,7 +29,7 @@ class App extends React.Component {
           <SearchBar />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults}/>
-            <Playlist />
+            <Playlist playlistTracks={this.state.playlistTracks} playlistName={this.state.playlistName}/>
           </div>
         </div>
       </div>
