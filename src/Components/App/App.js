c1606548ba21db3525686807d1b5c197cdca0f4d
bonnie-gaggle <bonnie@gaggle.net>
2023-02-22 15:42:31 -0800

    beginning of save functionality

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -23,6 +23,7 @@ class App extends React.Component {
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
   }
 
   addTrack(track) {
@@ -47,6 +48,11 @@ class App extends React.Component {
     this.setState({playlistName: name})
   }
 
+  savePlaylist() {
+    const trackUris = this.state.playlistTracks.map(track => track.uri);
+    // more later
+  }
+
   render() {
     return (
       <div>
@@ -63,6 +69,7 @@ class App extends React.Component {
               playlistName={this.state.playlistName}
               onRemove={this.removeTrack}
               onNameChange={this.updatePlaylistName}
+              onSave={this.savePlaylist}
             />
           </div>
         </div>
