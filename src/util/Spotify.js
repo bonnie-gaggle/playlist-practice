ec5be9758e39476e834f8ec9bef4bf09f14bd522
bonnie-gaggle <bonnie@gaggle.net>
2023-02-22 16:05:12 -0800

    add access token, redirect uri to Spotify module

---
+++ src/util/Spotify.js
@@ -0,0 +1,29 @@
+const clientId = 'e6004135dddc4dc3bfc35096daa4689b';
+const redirectUri = 'http://localhost:3000';
+let accessToken;
+
+const Spotify = {
+    getAccessToken() {
+        if (accessMatch) {
+            return accessMatch
+        }
+        const url = window.location.href;
+        const accessMatch = url.match(/access_token=([^&]*)/);
+        const expiryMatch = url.match(/expires_in=([^&]*)/);
+
+        if (accessMatch && expiryMatch) {
+            accessToken = accessMatch[1];
+            const expiresIn = Number(expiryMatch[1]);
+
+            // Wipe access token if it's expired:
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        }
+
+        const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectUri}`;
+        window.location = accessUrl;
+    }
+}
+
+export default Spotify;
